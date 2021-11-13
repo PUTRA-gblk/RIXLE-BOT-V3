db7480f34f1e2ddeadb68d2eff829653af559f39
PutraGanz <85389873+PUTRA-gblk@users.noreply.github.com>
2021-11-13 23:55:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ global.audio = mediaType.audio
 global.location = mediaType.location
 global.document = mediaType.document
 
-conn.version = [ 2, 2140, 12 ]
+conn.version = [ 2, 2143, 3 ]
 conn.logger.level = "warn"
 conn.browserDescription = ['Rixle Type 3', 'SAFARI', '8.1']
 
